062a3178c105cf885dd15647eb25afb343222f0b
Shamim-Nkatha <123588376+Shamim-Nkatha@users.noreply.github.com>
2023-02-23 22:56:52 +0300

    Add files via upload

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -4,4 +4,37 @@ function BlogContent(props) {
   return <div id="blog-content">{props.articleText}</div>;
 }
 
+function BlogContent(props) {
+  console.log(props);
+  
+  if (!props.isPublished) {
+    // hide unpublished content
+    // return null means "don't display any DOM elements here"
+    return null;
+  } else {
+    // show published content
+    return (
+      <div>
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead} minutes to read</p>
+      </div>
+    );
+}
+
+}
+/* 
+  { 
+    articleText: "Dear Reader: Bjarne Stroustrup has the perfect lecture oration.",
+    isPublished: true,
+    minutesToRead: 1 
+  }
+*/
+
+<BlogContent
+  articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration."
+  isPublished={true}
+  minutesToRead={1}
+/>
+
+
 export default BlogContent;
--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,16 +1,21 @@
-import React from "react";
-import BlogContent from "./BlogContent";
-import Comment from "./Comment";
+<BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
 
 function BlogPost() {
   return (
-    <div id="blog-post">
+    <div>
+      {/* BlogContent is being returned from BlogPost */}
+      {/* Therefore, BlogContent is a child of BlogPost */}
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+   
     </div>
   );
 }
 
-export default BlogPost;
+// BlogContent.js
+// CHILD COMPONENT
+function BlogContent(props) {
+  return <div>{props.articleText}</div>;
+}
